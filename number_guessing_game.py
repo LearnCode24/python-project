588b3c89adaafa7d982b1ba503e054b8b462eadf
Echii <learncodewithme247@gmail.com>
2026-09-02 13:37:06 +0530

    modified the code

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,18 +2,30 @@ import random
 import time
 #Random Number guessing game program 
 start=time.time()
-secret_number=random.randint(1,100)
-count=0
+highest=100
+lowest=1
+secret_number=random.randint(lowest,highest)
+guesses=0
+print("Python Random Guessing Number Game")
+print(f"Guess Number between {lowest} and {highest}.")
 while True:
-    number=int(input("Enter a secret number:"))
-    count+=1
-    if number>secret_number:
-        print("Too high!")
-    elif number<secret_number:
-        print("Too low!")
+    guess=input("Enter a number:")
+    if guess.isdigit():
+        guess=int(guess)
+        guesses+=1
+        if guess>secret_number:
+            print("Too high! Try again.")
+        elif guess<secret_number:
+            print("Too low! Try again.")
+        else:
+            print(f"Correct! The number was {secret_number}.")
+            break
+
+        
     else:
-        print(f"You have guessed right! The Correct Number is {secret_number}")
-        break
-    
+        print("Invalid Number!")
+        print(f"Please guess Number between {lowest} and {highest}.")
+
 end=time.time()
-print(f"The no  of guesses and time you took is {count} and {end-start:.1f} seconds respectively.")
+time_taken=end-start
+print(f"The number guesses and time is {guesses} and {round(time_taken)} second.")
