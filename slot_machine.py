34463f130f9bd52e82b0609155d88688da3ab06a
Echii <learncodewithme247@gmail.com>
2026-09-20 12:30:49 +0530

    added some features

--- slot_machine.py
+++ slot_machine.py
@@ -3,13 +3,14 @@ import time
 
 SYMBOLS = ['🍒', '🍉', '🍋', '🔔', '⭐']
 
-def get_win_chance(bet, max_bet=100):
-    chance = 0.05 + (bet / max_bet) * 0.35
-    return min(chance, 0.40)
 
+def spin_row(bet,balance):
+    if balance<=0:
+        win_chance=0.0
+    else:
+        chance=0.05+(bet/balance)*0.85
+        win_chance=min(chance,0.90)
 
-def spin_row(bet):
-    win_chance = get_win_chance(bet)
     if random.random() < win_chance:
         symbol = random.choice(SYMBOLS)
         return [symbol, symbol, symbol]
@@ -20,11 +21,12 @@ def spin_row(bet):
                 return row
 
 
+    
+
 def print_row(row):
     
     time.sleep(0.5)
 
-    
     for i in range(8):
         flicker = [random.choice(SYMBOLS) for j in range(3)]
         print("| " + " | ".join(flicker) + " |", end="\r")
@@ -61,12 +63,11 @@ def get_payout(row, bet):
     return 0
 
 def main():
+    print("*************GAME START**************")
     balance = int(input("Enter your balance: "))
-    print("**************************************")
     while True:
         print(f"Your current balance Rs.{balance}.")
         bet = input("Enter your bet amount: ")
-        print("**************************************")
 
         if not bet.isdigit():
             print("Please, enter a valid amount!")
@@ -82,16 +83,18 @@ def main():
             print("Insufficient fund!")
             continue
 
+        
+        row = spin_row(bet,balance)
         balance -= bet
-        row = spin_row(bet)
+        print("*****************************************")
         print("Spinning....")
         print_row(row)
 
         payout = get_payout(row, bet)
         if payout > 0:
+            print("*****************************************")
             print(f"You won Rs.{payout}.")
         else:
-            print("**************************************")
             print("Sorry you lost this round!")
         balance += payout
         if balance==0:
@@ -107,9 +110,9 @@ def main():
         else:
             break
 
-    print("**************************************")
+    print("*****************************************")
     print(f"Game over! Your final balance is Rs.{balance}")
-    print("**************************************")
+    print("*****************************************")
 
 if __name__=="__main__":
     main()
